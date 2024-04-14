9a8be7bc6c596e0a592d8a13353990e7e7403965
Elxan777 <146763878+Elxan777@users.noreply.github.com>
2024-04-14 10:34:35 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,171 @@
+public class Main {
+    public static void main(String[] args) {
+//        for (int i = 1; i < 10; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println(i);
+//            }
+//        }
+//        for (int i = 1; i < 10; i++) {
+//            if (i % 2 !=0){
+//                System.out.println(i);
+//            }
+//        }
+//
+//        StringBuilder sb=new StringBuilder("Hello");
+//        sb.reverse();
+//        System.out.println(sb);
+
+//     for (int i = 1; i<=10; i++)
+//        System.out.println(i);
+
+//        int[] numbers = {1, 2, 3, 4, 5};
+//
+//        int toplam = 0;
+//        for (int num : numbers) {
+//            toplam += num;
+//        }
+//        System.out.println("Verilmis ededlerin cemi: " + toplam);
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Soz daxil edin: ");
+//        String scanner = scan.nextLine();
+//        String s = "";
+//
+//        for (int i = (scanner.length() - 1); i >= 0; i--) {
+//            s += scanner.charAt(i);
+//        }
+//        if (s.equals(scanner)) {
+//            System.out.println("Daxil edilen soz Palindrom sozdur.");
+//        } else {
+//            System.out.println("Daxil edilen soz Palindrom soz deyil");
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//        String input = scanner.nextLine();
+//        System.out.println("Bir simvol yazin");
+//
+//        int[] arr = {'a', 'e', 'i', 'o', 'u'};
+//        int samit = 0;
+//
+//        if (arr.length > samit) {
+//
+//            System.out.println("Daxil etdiyiniz simvol saitdir");
+//        } else {
+//            System.out.println("Daxil etdiyiniz simvol samitdir");
+//        }
+//        System.out.println("Salam Elxan");
+//        int a = -5;
+//        int b = 8;
+//        int c = 6;
+//        int i = 55;
+//        int d = 9;
+//        int f = 20;
+//        int e = -3;
+//        int g = 15;
+//        int h = 3;
+//        int j = 2;
+//        int l = 8;
+//        int s = 5;
+//
+//        int x= (a+b*c);
+//        int q = ((i+d)%d);
+//        int w = (f+e*s/l);
+//        int t = (s+g/h*j-l%h);
+//
+//        System.out.println(x);
+//        System.out.println(q);
+//        System.out.println(w);
+//        System.out.println(t);
+
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Input six non-negative digits: 123456 ");
+//        int input = scan.nextInt();
+//        int n1 = input / 100000 % 10;
+//        int n2 = input / 10000 % 10;
+//        int n3 = input / 1000 % 10;
+//        int n4 = input / 100 % 10;
+//        int n5 = input / 10 % 10;
+//        int n6 = input % 10;
+//        System.out.println(n1 + " " + n2 + " " + n3 + " " + n4 + " " + n5 + " " + n6);
+
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Birinci ededi daxil edin : ");
+//        double eded1 = scanner.nextDouble();
+//
+//        System.out.print("Ikinci ededi daxil edin:  ");
+//        double eded2 = scanner.nextDouble();
+//
+//        double topla = num1 + num2;
+//        double cix = eded1 - eded2;
+//        double vur = eded1 * eded2;
+//        double bol = eded1 / eded2;
+//
+//
+//        System.out.println("Netice: ");
+//        System.out.println("Topla: " + topla);
+//        System.out.println("Cix: " + cix);
+//        System.out.println("Vur: " + vur);
+//        System.out.println("Bol: " + bol);
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Bir eded daxil edin: ");
+//        int eded = scanner.nextInt();
+//
+//        if(eded % 2 == 0) {
+//            System.out.println(num + " bu eded cutdur");
+//        }else
+//            System.out.println(num + " bu eded tekdir");
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Bir eded daxil edin: ");
+//        int eded = scanner.nextInt();
+//
+//        if(eded % 2 == 0 && eded>100) {
+//            System.out.println("Ugurlu emeliyyat");
+//        } else
+//            System.out.println("Ugursuz emeliyyat");
+//
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("Bir eded daxil edin: ");
+//        int eded = new input.nextInt();
+//
+//        if (eded > 100) {
+//            int netice = eded + 15;
+//            System.out.println("Ugurlu emeliyyat"+netice);
+//        }else {
+//            System.out.println("Ugursuz emeliyyat");
+//        }
+//        input.close();
+//
+//        for (int i = 1; i < 6; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+//        for (int i = 1; i <5; i++) {
+//            for (int j = 1; j <10; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for (int i = 0; i < 10; i++){
+//            if (i==3) {
+//                continue;
+//            }
+//            System.out.println("i'nin deyeri= " + i);
+//        }
+//        for (int i = 0; i < 10; i++){
+//            if (i==5) {
+//                break;
+//            }
+//            System.out.println("i'nin deyeri= " + i);
+//        }
+    }
+}
